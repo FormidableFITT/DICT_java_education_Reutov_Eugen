2f28146139ac2710eb3bfc8e91aa44c670275c10
Formidable <y.r.reutov@student.khai.edu>
2021-11-01 15:08:45 +0200

    ChatBot 1 stage

---
+++ src/ChatBot/ChatBott.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+public class ChatBott {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Robert.");
+        System.out.println("I was created in 2021.");
+
+    }
+}
