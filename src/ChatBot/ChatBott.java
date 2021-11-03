a395ae0902eb52cfece288d34eeb8e1472c37bc1
Formidable <y.r.reutov@student.khai.edu>
2021-11-03 23:05:27 +0200

    ChatBot 2 stage

--- src/ChatBot/ChatBott.java
+++ src/ChatBot/ChatBott.java
@@ -1,9 +1,17 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBott {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Robert.");
         System.out.println("I was created in 2021.");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+        String name = in.nextLine();
+
+        System.out.printf("What a great name you have, %s \n", name);
+        in.close();
+
 
     }
 }
