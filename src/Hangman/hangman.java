01d49cac03013b007d7a89ebdc1e1d3db5d5d883
Formidable <y.r.reutov@student.khai.edu>
2021-11-21 22:04:53 +0200

    Hangman 4

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -9,8 +9,13 @@ public class hangman {
                 "The game will be available soon.");
         Scanner in = new Scanner(System.in);
         String word = WORDS[new Random().nextInt(WORDS.length)];
+        for(String i :word) {
+
+        }
+        String allGuessCharsStr = "";
         while(true)
         {   System.out.print("Guess the word:");
+            System.out.println(getMasked(word, allGuessCharsStr));
             String name = in.nextLine();
             if(word.equals(name) ){
                 System.out.println("You survived!");
@@ -22,4 +27,20 @@ public class hangman {
         in.close();
 
     }
+    private static final String getMasked(String secret_word, String all_guessesStrESIfNone)  {
+        try  {
+            if(all_guessesStrESIfNone.length() == 0)  {
+                all_guessesStrESIfNone = " ";   //Any non-letter will suffice
+            }
+        }  catch(NullPointerException npx)  {
+            throw  new NullPointerException("all_guessesStrESIfNone");
+        }
+
+        //Mask all not-yet-guessed characters with an underscore.
+        secret_word = secret_word.replaceAll("[^" + all_guessesStrESIfNone + "]", "_");
+
+        //Insert a space between every character (trim eliminates the final).
+        return  secret_word.replaceAll("(.)", "$1 ").trim();
+    }
 }
+
