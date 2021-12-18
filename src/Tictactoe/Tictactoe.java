6700f0a69faa07b1f82b99a181d2c1787c95a683
Formidable <y.r.reutov@student.khai.edu>
2021-12-18 18:10:02 +0200

    Tictactoe 1

---
+++ src/Tictactoe/Tictactoe.java
@@ -0,0 +1,10 @@
+package Tictactoe;
+import java.util.Scanner;
+
+public class Tictactoe {
+    public static void main(String[] args) {
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O");
+    }
+}
